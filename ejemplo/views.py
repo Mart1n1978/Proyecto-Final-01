8584a2224865a111a81e4a3e6668baebecad334c
Martin Giuliano <martingiuliano@gmail.com>
2022-12-08 14:38:41 -0300

    primer commit de mi proyecto

---
+++ ejemplo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "ejemplo/saludar.html") 
+
+def saludar_a(request, nombre):
+    return render (request,
+    'ejemplo/saludar_a.html',
+    {'nombre': nombre}
+    )
+
+def sumar(request, a, b):
+    return render (request,
+    'ejemplo/sumar.html',
+    {'a':a,
+    'b': b,
+    'resultado':a+b
+    }
+    )
+
+def buscar(request):
+    lista_de_nombre = ['German', 'Daniel', 'Martin']
+    query = request.GET['q']
+    if query in lista_de_nombre:
+        indice_de_resultado = lista_de_nombre.index(query)
+        resultado = lista_de_nombre[indice_de_resultado]
+    else:
+        resultado = 'no hay match'
+    return render(request, 'ejemplo/buscar.html', {'resultado': resultado})
