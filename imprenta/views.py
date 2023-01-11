ce63fb97412108a40a5f7334bd514068e1d174f9
Martin Giuliano <martingiuliano@gmail.com>
2023-01-11 01:34:09 -0300

    actualizacion

--- imprenta/views.py
+++ imprenta/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from imprenta.models import Post
+from django.urls import reverse_lazy
+
 
 def index(request):
         return render(request, "imprenta/index.html", {})
-# Create your views here.
+
+
+class PostList(ListView):
+        model = Post
+
+class PostCrear(CreateView):
+        model = Post
+        success_url = reverse_lazy("imprenta-listar")
+        fields =  '__all__'
+
